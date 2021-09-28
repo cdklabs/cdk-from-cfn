f3bef55051ab612ff9e47221f39c10e78c251dce
robelaks <robelaks@amazon.com>
2021-09-27 19:12:29 -0700

    Add logic to synthesize mappings to typescript. Refactor parsing

--- docs/3_lookups.md
+++ docs/3_lookups.md
@@ -1,10 +1,11 @@
 # Noctilucent Lookup Tables
 
-This document describes the breakdown of how to do lookup table resolution in Noctilucent.
+This document describes the breakdown of how the Mappings section of a CloudFormation template is converted to CDK 
+typescript in Noctilucent.
 
 ## Where does this fit? 
 
-See [1_overview.md](replace with ref to file) for information on where a Mapping resource in a CloudFormation template
+See [1_overview.md](./1_overview.md) for information on where a Mapping resource in a CloudFormation template
 will get transpiled to in the produced CDK.
 
 ## Example
@@ -36,56 +37,83 @@ const AardvarkFlowLogAccessRoleMappingProd Map<String, Map<String, String>> {
 
 and then all meta lookups into the table will be replaced with hashmap lookups.
 
-## Synthesis
+## High Level Overview
 
-There are two parts that need to be solved here. The first is to convert the Mapping structure in CloudFormation to CDK
-maps. The second problem is to expose the name of these maps so they can be used by conditions and resources that
-are generated at a later point.
+There are two parts that need to be solved here: Parsing and Synthesis. Parsing is responsible for converting the 
+Mappings to an internal representation. Synthesis is responsible for translating the internal representation to 
+CDK typescript that can be used in the rest of the CDK document.
 
-Each mapping consists of a name and a key-value pair, where the value is another map. This can be built use recursion.
-
-### Building Maps
+### Parsing
 
 Let's start with some basic terminology. Using the below example:
 
 ```
-"AardvarkFlowLogAccessRoleMappingProd": {
-      ````"ARN": {
-        "PrincipalArn": "118920060295:role/AardvarkFlowLogAccessRole",
+"AardvarkFlowLogAccessRoleMappingProd [1]": {
+      "ARN [2]": {
+        "PrincipalArn [3]": "118920060295:role/AardvarkFlowLogAccessRole [4]",
         "ResourceArnPrefix": "915915706018:role/AardvarkLambdaReadOnly"
-      },
-},
+      }[5],
+}[6],
 ...
 ```
 
-we create the following terms:
+we define the following terms:
 
-* `AardvarkFlowLogAccessRoleMappingProd` as **OuterMappingName**
-* `ARN` as **OuterMappingKey**
-* ```
-  {
+1. `AardvarkFlowLogAccessRoleMappingProd` as **OuterMappingName**
+2. `ARN` as **OuterMappingKey**
+3. `PrincipalArn` as **InnerMappingKey**
+4. `118920060295:role/AardvarkFlowLogAccessRole` as **InnerMappingValue**
+5. 
+```
+{
   "PrincipalArn": "118920060295:role/AardvarkFlowLogAccessRole",
   "ResourceArnPrefix": "915915706018:role/AardvarkLambdaReadOnly"
-  }
-  ``` 
-  as **InnerMapping** 
+}
+``` 
+   as **InnerMapping**
+```
+"AardvarkFlowLogAccessRoleMappingProd [1]": {
+      "ARN [2]": {
+        "PrincipalArn [3]": "118920060295:role/AardvarkFlowLogAccessRole [4]",
+        "ResourceArnPrefix": "915915706018:role/AardvarkLambdaReadOnly"
+      }[5],
+}[6]
+```
+   as **OuterMapping**
 
 The basic flow looks something like:
 
-1. Create tracker for all mappings, structure looks like: Map<String, Map<String, Map<String, String>>
+1. Create tracker for all mappings, structure looks like: Map<String, Map<String, Map<String, String|List<String>>>
 2. Start tracking for new outer mapping with key set to OuterMappingName
-   1. Add new key-value entry, where
-      1. key name matches OuterMappingKey
-      2. Value is a Map<String, String> that will represent the InnerMapping
-   2. Iterate through entries in the InnerMapping add them to Map assigned as value of the OuterMappingKey
+   1. Build a map entry, where
+      1. Key name matches OuterMappingKey
+      2. Value is a Map<String, String|List<String>> that will represent the InnerMapping
+         1. Iterate through entries in the InnerMapping add them to internal Map representation
 3. Repeat for next mapping
 
-### Publish
+### Synthesis
 
-After all maps are created the CDK will be generated as follows:
+After all maps are created the CDK typescript mapping will be generated 
 
 1. Iterate through entry-set of mappings
-2. Declare const with name matching key
-3. Set value of const to be a dump of the OuterMapping represented by its value
+   1. Start mapping declaration as `const <name> = ` where name is the key (OuterMappingName) from the entry-set
+   2. Construct value from entry-set value (OuterMapping) as
+      1. Start value `new Map(`
+      2. Iterate through entry-set of OuterMapping
+         1. Start entry `[`
+         2. Add key `\"<key>\"` where key is OuterMappingKey
+         3. Start value `new Map(`
+         4. Iterate through entry-set of InnerMapping
+            1. Start entry `[`
+            2. Add key `\"<key>\"` where key is InnerMappingKey
+            3. Add value where value is either:
+               1. `\"<value>\"` - simple string value, or
+               2. `[\"<value1>\",\"<value2>\",...,\"<valueN>\"]` - List of strings
+            4. End entry `]`
+         5. End value `)`
+         6. End entry `]`
+      3. End value `)`
+
+  
 
 
--- src/parser/lookup_table.rs
+++ src/parser/lookup_table.rs
@@ -1,40 +1,114 @@
 use crate::TransmuteError;
 use serde_json::{Map, Value};
 use std::collections::HashMap;
+use std::fmt::{Display, Formatter};
 
 #[derive(Debug)]
 pub struct MappingsParseTree {
-    pub mappings: HashMap<String, HashMap<String, HashMap<String, MappingInnerValue>>>,
+    pub mappings: HashMap<String, MappingParseTree>
+}
+
+impl MappingsParseTree {
+    pub fn new() -> MappingsParseTree {
+        return MappingsParseTree {
+            mappings: HashMap::new()
+        }
+    }
+
+    pub fn insert(&mut self, mapping_name: String, mapping: MappingParseTree) {
+        self.mappings.insert(mapping_name, mapping);
+    }
+
+    pub fn synthesize(&self) -> String {
+        let mut mappings_ts_str = String::new();
+        for (mapping_name, mapping) in self.mappings.iter() {
+            mappings_ts_str.push_str(&format!("const {} = {}", mapping_name, mapping.synthesize()));
+        }
+        return mappings_ts_str;
+    }
 }
 
 #[derive(Debug)]
-pub enum MappingInnerValue {
+pub struct MappingParseTree {
+    mappings: HashMap<String, HashMap<String, MappingInnerValue>>
+}
+
+impl MappingParseTree {
+    fn new() -> MappingParseTree {
+        return MappingParseTree {
+            mappings: HashMap::new()
+        }
+    }
+
+    fn insert(&mut self, outer_mapping_key: String, inner_mapping: HashMap<String, MappingInnerValue>) {
+        self.mappings.insert(outer_mapping_key, inner_mapping);
+    }
+
+    fn synthesize(&self) -> String {
+        let mut mapping_parse_tree_ts = String::new();
+        mapping_parse_tree_ts.push_str(&format!("new Map(\n"));
+        for (outer_mapping_key, inner_mapping) in self.mappings.iter() {
+            mapping_parse_tree_ts.push_str(&format!("\t[{}\t],\n", synthesize_outer_mapping(outer_mapping_key, inner_mapping)));
+        }
+        mapping_parse_tree_ts.push_str(&format!(")\n"));
+        return mapping_parse_tree_ts;
+    }
+}
+
+fn synthesize_outer_mapping(outer_mapping_entry : &String, inner_mapping: &HashMap<String, MappingInnerValue>) -> String {
+    format!("\"{}\", {}", outer_mapping_entry, synthesize_inner_mapping(inner_mapping))
+}
+
+fn synthesize_inner_mapping(inner_mapping: &HashMap<String, MappingInnerValue>) -> String {
+    let mut inner_mapping_ts_str = String::from("new Map(\n");
+    let mut inner_mapping_entries = Vec::new();
+    for (inner_mapping_key, inner_mapping_value) in inner_mapping {
+        inner_mapping_entries.push(format!("\t\t[\"{}\", {}]", inner_mapping_key, inner_mapping_value));
+    }
+    inner_mapping_ts_str.push_str(&inner_mapping_entries.join(",\n"));
+    inner_mapping_ts_str.push_str(")\n");
+    return inner_mapping_ts_str;
+}
+
+/**
+ * MappingInnerValue tracks the allowed value types in a Mapping as defined by CloudFormation in the
+ * link below. Right now that is either a String or List.
+ *
+ * https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/mappings-section-structure.html#mappings-section-structure-syntax
+ */
+#[derive(Debug)]
+enum MappingInnerValue {
     String(String),
     List(Vec<String>),
 }
 
+impl Display for MappingInnerValue {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            MappingInnerValue::String(string_val) => {
+                return write!(f, "\"{}\"", string_val);
+            }
+            MappingInnerValue::List(list_val) => {
+                let quoted_list_values : Vec<String> = list_val.iter().map(|val| format!("\"{}\"", val)).collect();
+                return write!(f, "[{}]", quoted_list_values.join(","));
+            }
+        }
+    }
+}
+
 pub fn build_mappings(vals: &Map<String, Value>) -> Result<MappingsParseTree, TransmuteError> {
-    let mut mappings = MappingsParseTree {
-        mappings: HashMap::new(),
-    };
+    let mut mappings = MappingsParseTree::new();
     for (name, obj) in vals {
         let outer_mapping = build_outer_mapping(name, obj)?;
-        mappings.mappings.insert(name.clone(), outer_mapping);
+        mappings.insert(name.clone(), outer_mapping);
     }
     Ok(mappings)
 }
 
-fn build_outer_mapping(name: &str, obj: &Value) -> Result<HashMap<String, HashMap<String, MappingInnerValue>>, TransmuteError> {
-    let val = match obj {
-        Value::Object(x) => x,
-        _ => {
-            return Err(TransmuteError {
-                details: format!("Mapping must be an object {}, {:?}", name, obj),
-            })
-        }
-    };
+fn build_outer_mapping(name: &String, obj: &Value) -> Result<MappingParseTree, TransmuteError> {
+    let val = ensure_object(name, obj)?;
 
-    let mut outer_mapping: HashMap<String, HashMap<String, MappingInnerValue>> = HashMap::new();
+    let mut outer_mapping: MappingParseTree = MappingParseTree::new();
     #[allow(clippy::never_loop)]
     for (outer_key, inner_mapping_obj) in val {
         let inner_mapping = build_inner_mapping(outer_key, inner_mapping_obj)?;
@@ -43,30 +117,13 @@ fn build_outer_mapping(name: &str, obj: &Value) -> Result<HashMap<String, HashMa
     return Ok(outer_mapping);
 }
 
-fn build_inner_mapping(name: &str, obj: &Value) -> Result<HashMap<String, MappingInnerValue>, TransmuteError> {
-    let val = match obj {
-        Value::Object(x) => x,
-        _ => {
-            return Err(TransmuteError {
-                details: format!("Mapping must be an object. Found {}, {:?}", name, obj),
-            })
-        }
-    };
+fn build_inner_mapping(name: &String, obj: &Value) -> Result<HashMap<String, MappingInnerValue>, TransmuteError> {
+    let val = ensure_object(name, obj)?;
 
     let mut inner_mapping: HashMap<String, MappingInnerValue> = HashMap::new();
     #[allow(clippy::never_loop)]
     for (inner_key, inner_mapping_obj) in val {
-        let val  = match inner_mapping_obj {
-            Value::String(x) => MappingInnerValue::String(x.to_string()),
-            Value::Number(x) => MappingInnerValue::String(x.to_string()),
-            Value::Array(x) => MappingInnerValue::List(convert_to_string_vector(x, inner_key)?),
-            _ => {
-                return Err(TransmuteError {
-                    details: format!("Inner mapping value must be a string or array. Found {:?}, for {}", name, inner_mapping_obj),
-                })
-            }
-        };
-
+        let val  = ensure_mapping_value_type(inner_key, inner_mapping_obj)?;
         inner_mapping.insert(inner_key.clone(), val);
     }
     return Ok(inner_mapping);
@@ -87,6 +144,30 @@ fn convert_to_string_vector(json_vector: &Vec<Value>, inner_key: &String) -> Res
         string_vector.push(converted_val);
     }
     return Ok(string_vector);
+}
+
+fn ensure_object<'a>(name: &String, obj: &'a Value) -> Result<&'a Map<String, Value>, TransmuteError> {
+    return match obj {
+        Value::Object(x) => Ok(x),
+        _ => {
+            Err(TransmuteError {
+                details: format!("Mapping must be an object {}, {:?}", name, obj),
+            })
+        }
+    };
+}
+
+fn ensure_mapping_value_type(name: &String, obj: &Value) -> Result<MappingInnerValue, TransmuteError> {
+    return match obj {
+        Value::String(x) => Ok(MappingInnerValue::String(x.to_string())),
+        Value::Number(x) => Ok(MappingInnerValue::String(x.to_string())),
+        Value::Array(x) => Ok(MappingInnerValue::List(convert_to_string_vector(x,  name)?)),
+        _ => {
+            Err(TransmuteError {
+                details: format!("Inner mapping value must be a string or array. Found {:?}, for {}", name, obj),
+            })
+        }
+    };
 }
 
 
