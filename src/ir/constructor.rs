49c1aa1655a0002128103a3effa898e180ecfdfb
Naumel <naume@amazon.com>
2023-05-09 17:42:28 +0200

    unit tests

--- src/ir/constructor.rs
+++ src/ir/constructor.rs
@@ -63,12 +63,21 @@ mod tests {
         assert_eq!(constructor.inputs.len(), 2);
 
         assert_eq!(constructor.inputs[0].name, "param1");
-        assert_eq!(constructor.inputs[0].description, Some("description1".to_string()));
+        assert_eq!(
+            constructor.inputs[0].description,
+            Some("description1".to_string())
+        );
         assert_eq!(constructor.inputs[0].constructor_type, "String");
-        assert_eq!(constructor.inputs[0].default_value, Some("default1".to_string()));
+        assert_eq!(
+            constructor.inputs[0].default_value,
+            Some("default1".to_string())
+        );
 
         assert_eq!(constructor.inputs[1].name, "param2");
-        assert_eq!(constructor.inputs[1].description, Some("description2".to_string()));
+        assert_eq!(
+            constructor.inputs[1].description,
+            Some("description2".to_string())
+        );
         assert_eq!(constructor.inputs[1].constructor_type, "Number");
         assert_eq!(constructor.inputs[1].default_value, None);
     }
