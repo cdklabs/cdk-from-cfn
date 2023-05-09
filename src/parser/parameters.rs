6dc8a2bd63a2d19fcd95818fc93e174b8ea68998
Naumel <104374999+Naumel@users.noreply.github.com>
2023-05-09 16:58:33 +0200

    Update parameters.rs
    Added unit tests

--- src/parser/parameters.rs
+++ src/parser/parameters.rs
@@ -32,3 +32,55 @@ impl fmt::Display for ParameterType {
         }
     }
 }
+        
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_yaml::Mapping;
+
+    #[test]
+    fn test_add_parameter() {
+        let mut params = Parameters::new();
+        params.add(Parameter::new(
+            "p1".to_string(),
+            "Number".to_string(),
+            Option::from("0".to_string()),
+        ));
+        let result: &Parameter = params.params.get("p1").unwrap();
+        assert_eq!(result.logical_name, "p1".to_string());
+        assert_eq!(result.default, Option::from("0".to_string()));
+        assert_eq!(result.parameter_type, "Number".to_string());
+    }
+
+    #[test]
+    fn test_build_parameters() {
+        let yaml_text = r#"
+        Param1:
+            Type: String
+        Param2:
+            Type: Number
+            Default: "0"
+          "#;
+
+        let map: Mapping = serde_yaml::from_str(&yaml_text).unwrap();
+        let params = build_parameters(&map).unwrap();
+
+        assert_eq!(
+            params,
+            Parameters {
+                params: {
+                    let mut map = HashMap::new();
+                    map.insert(
+                        "Param1".into(),
+                        Parameter::new("Param1".into(), "String".into(), None),
+                    );
+                    map.insert(
+                        "Param2".into(),
+                        Parameter::new("Param2".into(), "Number".into(), Some("0".into())),
+                    );
+                    map
+                }
+            }
+        );
+    }
+}
