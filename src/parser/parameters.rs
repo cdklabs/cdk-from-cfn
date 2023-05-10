1ca198ef9caecdaaa8a43b7e6b9df56e37280141
Naumel <104374999+Naumel@users.noreply.github.com>
2023-05-10 10:37:39 +0200

    chore: Added unit tests (#127)

--- src/parser/parameters.rs
+++ src/parser/parameters.rs
@@ -32,3 +32,23 @@ impl fmt::Display for ParameterType {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parameter_type_display() {
+        assert_eq!(ParameterType::String.to_string(), "String");
+        assert_eq!(ParameterType::Number.to_string(), "Number");
+        assert_eq!(ParameterType::ListOfNumbers.to_string(), "List<Number>");
+        assert_eq!(
+            ParameterType::CommaDelimitedList.to_string(),
+            "CommaDelimitedList"
+        );
+        assert_eq!(
+            ParameterType::Other("CustomType".to_string()).to_string(),
+            "CustomType"
+        );
+    }
+}
