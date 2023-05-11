e129e9f6a84f0f119f08cd821b3a1f13f080fe46
Naumel <104374999+Naumel@users.noreply.github.com>
2023-05-11 15:19:51 +0200

    chore: tests for skipped lines (#131)

--- src/primitives/mod.rs
+++ src/primitives/mod.rs
@@ -54,3 +54,35 @@ impl From<u128> for WrapperF64 {
         WrapperF64::new(num as f64)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_u64() {
+        let value: u64 = 42;
+        let wrapper: WrapperF64 = value.into();
+        let expected: WrapperF64 = WrapperF64::new(42.0);
+
+        assert_eq!(wrapper, expected);
+    }
+
+    #[test]
+    fn test_from_i128() {
+        let value: i128 = -10;
+        let wrapper: WrapperF64 = value.into();
+        let expected: WrapperF64 = WrapperF64::new(-10.0);
+
+        assert_eq!(wrapper, expected);
+    }
+
+    #[test]
+    fn test_from_u128() {
+        let value: u128 = 1000;
+        let wrapper: WrapperF64 = value.into();
+        let expected: WrapperF64 = WrapperF64::new(1000.0);
+
+        assert_eq!(wrapper, expected);
+    }
+}
