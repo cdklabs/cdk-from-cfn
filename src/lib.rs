c0e1a87ab42d04e979a643a46968de65e510535f
🧑🏻‍💻 Romain Marcadier <rmuller@amazon.com>
2023-06-06 13:35:06 +0200

    add function to list supported languages

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,6 @@ use parser::lookup_table::MappingTable;
 use parser::output::Output;
 use parser::parameters::Parameter;
 use parser::resource::ResourceAttributes;
-use wasm_bindgen::prelude::*;
 
 pub mod errors;
 pub mod ir;
@@ -40,28 +39,48 @@ pub struct CloudformationParseTree {
     pub resources: IndexMap<String, ResourceAttributes>,
 }
 
-#[wasm_bindgen]
-pub fn transmute(template: &str, language: &str) -> Result<String, JsError> {
-    let cfn_tree: CloudformationParseTree = serde_yaml::from_str(template)?;
-    let ir = crate::ir::CloudformationProgramIr::from(cfn_tree)?;
-    let mut output = Vec::new();
-
-    let synthesizer: Box<dyn crate::synthesizer::Synthesizer> = match language {
-        #[cfg(feature = "typescript")]
-        "typescript" => Box::new(crate::synthesizer::Typescript {}),
-        #[cfg(feature = "golang")]
-        "go" => Box::<crate::synthesizer::Golang>::default(),
-        unsupported => panic!("unsupported language: {}", unsupported),
-    };
-
-    ir.synthesize(synthesizer.as_ref(), &mut output)?;
-
-    String::from_utf8(output).map_err(Into::into)
-}
-
 #[cfg(target_family = "wasm")]
-#[cfg(feature = "console_error_panic_hook")]
-#[wasm_bindgen(start)]
-fn wasm_init() {
-    console_error_panic_hook::set_once();
+pub mod wasm {
+    use wasm_bindgen::prelude::*;
+
+    use super::*;
+
+    /// Returns an array containing all supported language names.
+    #[wasm_bindgen]
+    pub fn supported_languages() -> Box<[JsValue]> {
+        vec![
+            #[cfg(feature = "typescript")]
+            wasm_bindgen::intern("typescript").into(),
+            #[cfg(feature = "golang")]
+            wasm_bindgen::intern("go").into(),
+        ]
+        .into_boxed_slice()
+    }
+
+    /// Transforms the provided template into a CDK application in the specified
+    /// language.
+    #[wasm_bindgen]
+    pub fn transmute(template: &str, language: &str) -> Result<String, JsError> {
+        let cfn_tree: CloudformationParseTree = serde_yaml::from_str(template)?;
+        let ir = crate::ir::CloudformationProgramIr::from(cfn_tree)?;
+        let mut output = Vec::new();
+
+        let synthesizer: Box<dyn crate::synthesizer::Synthesizer> = match language {
+            #[cfg(feature = "typescript")]
+            "typescript" => Box::new(crate::synthesizer::Typescript {}),
+            #[cfg(feature = "golang")]
+            "go" => Box::<crate::synthesizer::Golang>::default(),
+            unsupported => panic!("unsupported language: {}", unsupported),
+        };
+
+        ir.synthesize(synthesizer.as_ref(), &mut output)?;
+
+        String::from_utf8(output).map_err(Into::into)
+    }
+
+    #[cfg(feature = "console_error_panic_hook")]
+    #[wasm_bindgen(start)]
+    fn wasm_init() {
+        console_error_panic_hook::set_once();
+    }
 }
