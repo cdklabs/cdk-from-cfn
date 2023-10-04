c636241d6e0ad9ee5a009795cdad4f9b7834b179
Kendra Neil <53584728+TheRealAmazonKendra@users.noreply.github.com>
2023-10-04 06:48:23 -0700

    fix(python): pretty_name is not snake_case (#352)
    The function pretty_name is not handling the casing correctly. This change uses the snake_case function to correct this.
    
    Co-authored-by: Hogan Bobertz <Bobertzx@gmail.com>

---
+++ tests/end-to-end/documentdb/app.py
@@ -0,0 +1,57 @@
+from aws_cdk import Stack
+import aws_cdk as cdk
+import aws_cdk.aws_docdb as docdb
+from constructs import Construct
+
+"""
+  AWS CloudFormation Sample Template DocumentDB_Quick_Create: Sample template showing how to create a DocumentDB DB cluster and DB instance. **WARNING** This template creates an Amazon DocumentDB resources and you will be billed for the AWS resources used if you create a stack from this template.
+"""
+class DocumentDbStack(Stack):
+  def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    super().__init__(scope, construct_id, **kwargs)
+
+    # Applying default props
+    props = {
+      'dbClusterName': kwargs.get('dbClusterName', 'MyCluster'),
+      'dbInstanceName': kwargs.get('dbInstanceName', 'MyInstance'),
+    }
+
+    # Resources
+    dbCluster = docdb.CfnDBCluster(self, 'DBCluster',
+          db_cluster_identifier = props['dbClusterName'],
+          master_username = props['masterUser'],
+          master_user_password = props['masterPassword'],
+          engine_version = '4.0.0',
+        )
+    dbCluster.cfn_options.deletion_policy = cdk.CfnDeletionPolicy.DELETE
+
+    dbInstance = docdb.CfnDBInstance(self, 'DBInstance',
+          db_cluster_identifier = dbCluster.ref,
+          db_instance_identifier = props['dbInstanceName'],
+          db_instance_class = props['dbInstanceClass'],
+        )
+    dbInstance.add_dependency(dbCluster)
+
+    # Outputs
+    self.cluster_id = dbCluster.ref
+    cdk.CfnOutput(self, 'ClusterId', 
+      value = str(self.cluster_id),
+    )
+
+    self.cluster_endpoint = dbCluster.attr_endpoint
+    cdk.CfnOutput(self, 'ClusterEndpoint', 
+      value = str(self.cluster_endpoint),
+    )
+
+    self.cluster_port = dbCluster.attr_port
+    cdk.CfnOutput(self, 'ClusterPort', 
+      value = str(self.cluster_port),
+    )
+
+    self.engine_version = '4.0.0'
+    cdk.CfnOutput(self, 'EngineVersion', 
+      value = str(self.engine_version),
+    )
+
+
+
