b54b136b7c0d848c8897211d2cb4d3f9cde503ab
Francis <colifran@amazon.com>
2024-03-29 15:38:47 -0700

    formatting
    Signed-off-by: Francis <colifran@amazon.com>

--- src/synthesizer/csharp/tests.rs
+++ src/synthesizer/csharp/tests.rs
@@ -90,8 +90,7 @@ fn test_resource_ir_double() {
 fn test_resource_ir_select() {
     let output = CodeBuffer::default();
     let schema = Cow::Borrowed(Schema::builtin());
-    let resource_ir =
-        ResourceIr::Select(1, Box::new(ResourceIr::String("Not an array".into())));
+    let resource_ir = ResourceIr::Select(1, Box::new(ResourceIr::String("Not an array".into())));
     let result = resource_ir.emit_csharp(&output, &schema);
     assert_eq!((), result.unwrap());
 }
@@ -111,38 +110,38 @@ fn test_resource_ir_cidr() {
 
 #[test]
 fn test_invalid_resource_object_structure() {
-  let output = CodeBuffer::default();
-  let schema = Cow::Borrowed(Schema::builtin());
-  let resource_ir = ResourceIr::Object(
-    TypeReference::Union(TypeUnion::Static(&[])),
-    IndexMap::default(),
-  );
-  let result = resource_ir.emit_csharp(&output, &schema).unwrap_err();
-  let expected = "Type reference Union(\n    Static(\n        [],\n    ),\n) not implemented for ResourceIr::Object";
-  assert_eq!(expected, result.to_string());
+    let output = CodeBuffer::default();
+    let schema = Cow::Borrowed(Schema::builtin());
+    let resource_ir = ResourceIr::Object(
+        TypeReference::Union(TypeUnion::Static(&[])),
+        IndexMap::default(),
+    );
+    let result = resource_ir.emit_csharp(&output, &schema).unwrap_err();
+    let expected = "Type reference Union(\n    Static(\n        [],\n    ),\n) not implemented for ResourceIr::Object";
+    assert_eq!(expected, result.to_string());
 }
 
 #[test]
 fn test_invalid_resource_object_primitive() {
-  let output = CodeBuffer::default();
-  let schema = Cow::Borrowed(Schema::builtin());
-  let resource_ir = ResourceIr::Object(
-    TypeReference::Primitive(Primitive::String),
-    IndexMap::default(),
-  );
-  let result = resource_ir.emit_csharp(&output, &schema).unwrap_err();
-  let expected = "Cannot emit ResourceIr::Object with non-json simple structure (String)";
-  assert_eq!(expected, result.to_string());
+    let output = CodeBuffer::default();
+    let schema = Cow::Borrowed(Schema::builtin());
+    let resource_ir = ResourceIr::Object(
+        TypeReference::Primitive(Primitive::String),
+        IndexMap::default(),
+    );
+    let result = resource_ir.emit_csharp(&output, &schema).unwrap_err();
+    let expected = "Cannot emit ResourceIr::Object with non-json simple structure (String)";
+    assert_eq!(expected, result.to_string());
 }
 
 #[test]
 fn test_invalid_organization() {
-  let bad_org = "NotAws";
-  let import_instruction = ImportInstruction {
-    organization: bad_org.to_string(),
-    service: Option::None,
-  };
-  let result = import_instruction.to_csharp().unwrap_err();
-  let expected = format!("Expected organization to be AWS or Alexa. Found {bad_org}");
-  assert_eq!(expected, result.to_string());
+    let bad_org = "NotAws";
+    let import_instruction = ImportInstruction {
+        organization: bad_org.to_string(),
+        service: Option::None,
+    };
+    let result = import_instruction.to_csharp().unwrap_err();
+    let expected = format!("Expected organization to be AWS or Alexa. Found {bad_org}");
+    assert_eq!(expected, result.to_string());
 }
--- src/synthesizer/typescript/tests.rs
+++ src/synthesizer/typescript/tests.rs
@@ -13,12 +13,12 @@ fn pretty_name_fixes() {
 
 #[test]
 fn test_invalid_organization() {
-  let bad_org = "NotAws";
-  let import_instruction = ImportInstruction {
-    organization: bad_org.to_string(),
-    service: Option::None,
-  };
-  let result = import_instruction.to_typescript().unwrap_err();
-  let expected = format!("Expected organization to be AWS or Alexa. Found {bad_org}");
-  assert_eq!(expected, result.to_string());
+    let bad_org = "NotAws";
+    let import_instruction = ImportInstruction {
+        organization: bad_org.to_string(),
+        service: Option::None,
+    };
+    let result = import_instruction.to_typescript().unwrap_err();
+    let expected = format!("Expected organization to be AWS or Alexa. Found {bad_org}");
+    assert_eq!(expected, result.to_string());
 }
