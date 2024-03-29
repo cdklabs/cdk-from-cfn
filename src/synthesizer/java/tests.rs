5d85c0c8fb9ea2617d82787e42a1c53dd75db7b3
Francis <colifran@amazon.com>
2024-03-29 16:08:47 -0700

    java unit tests
    Signed-off-by: Francis <colifran@amazon.com>

--- src/synthesizer/java/tests.rs
+++ src/synthesizer/java/tests.rs
@@ -1,4 +1,12 @@
+use super::*;
+
+use std::borrow::Cow;
+
+use crate::cdk::Schema;
+use crate::code::CodeBuffer;
 use crate::ir::importer::ImportInstruction;
+use crate::ir::resources::ResourceIr;
+use crate::primitives::WrapperF64;
 
 #[test]
 fn test_invalid_organization() {
@@ -10,4 +18,41 @@ fn test_invalid_organization() {
     let result = import_instruction.to_java_import().unwrap_err();
     let expected = format!("Expected organization to be AWS or Alexa. Found {bad_org}");
     assert_eq!(expected, result.to_string());
+}
+
+#[test]
+fn test_alexa_organization() {
+    let import_instruction = ImportInstruction {
+        organization: "Alexa".to_string(),
+        service: Option::None,
+    };
+    let result = import_instruction.to_java_import();
+    assert_eq!("import Alexa.*;", result.unwrap());
+}
+
+#[test]
+fn test_resource_ir_bool() {
+    let output = CodeBuffer::default();
+    let schema = Cow::Borrowed(Schema::builtin());
+    let resource_ir = ResourceIr::Bool(true);
+    let result = emit_java(resource_ir, &output, Option::None, &schema);
+    assert_eq!((), result.unwrap());
+}
+
+#[test]
+fn test_resource_ir_number() {
+    let output = CodeBuffer::default();
+    let schema = Cow::Borrowed(Schema::builtin());
+    let resource_ir = ResourceIr::Number(10);
+    let result = emit_java(resource_ir, &output, Option::None, &schema);
+    assert_eq!((), result.unwrap());
+}
+
+#[test]
+fn test_resource_ir_double() {
+    let output = CodeBuffer::default();
+    let schema = Cow::Borrowed(Schema::builtin());
+    let resource_ir = ResourceIr::Double(WrapperF64::new(2.0));
+    let result = emit_java(resource_ir, &output, Option::None, &schema);
+    assert_eq!((), result.unwrap());
 }
