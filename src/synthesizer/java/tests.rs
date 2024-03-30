471697e9f30a13082f8f0a827c51598ff3f96872
Francis <colifran@amazon.com>
2024-03-29 19:15:02 -0700

    more java unit tests
    Signed-off-by: Francis <colifran@amazon.com>

--- src/synthesizer/java/tests.rs
+++ src/synthesizer/java/tests.rs
@@ -119,3 +119,15 @@ fn test_resource_ir_select_idx_greater_than_list_len() {
     let result = emit_java(resource_ir, &output, Option::None, &schema);
     assert_eq!((), result.unwrap());
 }
+
+#[test]
+fn test_resource_ir_split_non_string() {
+    let output = CodeBuffer::default();
+    let schema = Cow::Borrowed(Schema::builtin());
+    let resource_ir = ResourceIr::Split(
+        "-".to_string(),
+        Box::new(ResourceIr::Null),
+    );
+    let result = emit_java(resource_ir, &output, Option::None, &schema);
+    assert_eq!((), result.unwrap());
+}
