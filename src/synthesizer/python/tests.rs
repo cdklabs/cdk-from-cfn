7fbf2bcd67e919d024c25bf5750d78cdb06f89eb
Francis <colifran@amazon.com>
2024-03-29 20:21:17 -0700

    python unit tests
    Signed-off-by: Francis <colifran@amazon.com>

--- src/synthesizer/python/tests.rs
+++ src/synthesizer/python/tests.rs
@@ -1,4 +1,6 @@
-use crate::ir::importer::ImportInstruction;
+use crate::ir::{conditions::ConditionIr, importer::ImportInstruction};
+
+use super::synthesize_condition_recursive;
 
 #[test]
 fn test_invalid_organization() {
@@ -10,4 +12,23 @@ fn test_invalid_organization() {
     let result = import_instruction.to_python().unwrap_err();
     let expected = format!("Expected organization to be AWS or Alexa. Found {bad_org}");
     assert_eq!(expected, result.to_string());
+}
+
+#[test]
+fn test_alexa_org() {
+    let import_instruction = ImportInstruction {
+        organization: "Alexa".into(),
+        service: Some("Ask".into()),
+    };
+    let result = import_instruction.to_python();
+    assert_eq!("import alexa_ask as ask from ask", result.unwrap());
+}
+
+#[test]
+fn test_condition_ir_not_simple() {
+    let condition_ir = ConditionIr::Not(
+        Box::new(ConditionIr::Condition("condition".into())),
+    );
+    let result = synthesize_condition_recursive(&condition_ir);
+    assert_eq!("not (condition)", result);
 }
