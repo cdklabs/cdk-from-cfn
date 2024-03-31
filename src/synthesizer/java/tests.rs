692696ef565e977c775b5bf45d7f9a3476e80e75
Francis <colifran@amazon.com>
2024-03-30 21:50:25 -0700

    resource ir cidr string unit test hava
    Signed-off-by: Francis <colifran@amazon.com>

--- src/synthesizer/java/tests.rs
+++ src/synthesizer/java/tests.rs
@@ -141,3 +141,16 @@ fn test_resource_ir_cidr_null_mask() {
     let result = emit_java(resource_ir, &output, Option::None, &schema);
     assert_eq!((), result.unwrap());
 }
+
+#[test]
+fn test_resource_ir_cidr_string_mask() {
+    let output = CodeBuffer::default();
+    let schema = Cow::Borrowed(Schema::builtin());
+    let resource_ir = ResourceIr::Cidr(
+        Box::new(ResourceIr::String("0.0.0.0".into())),
+        Box::new(ResourceIr::String("16".into())),
+        Box::new(ResourceIr::String("255.255.255.0".into())),
+    );
+    let result = emit_java(resource_ir, &output, Option::None, &schema);
+    assert_eq!((), result.unwrap());
+}
