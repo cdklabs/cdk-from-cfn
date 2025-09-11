47f7434f410ef0b049ba34489ad3c3a6fe63d60e
Kendra Neil <53584728+TheRealAmazonKendra@users.noreply.github.com>
2025-09-11 00:29:41 -0700

    files moves and deletions on end-to-end test cases to reduce noise of actual changes

--- tests/end-to-end/app-boilerplate-files/typescript/setup-and-synth.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eu
-
-echo "npm install"
-npm install --no-package-lock
-
-echo "cdk synth"
-npx --yes cdk@latest synth --no-version-reporting --no-path-metadata --app 'npx ts-node ./app.ts'
